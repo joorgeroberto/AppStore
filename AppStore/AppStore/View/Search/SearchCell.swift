193406d5c8a6c004f0fa5205bec8cb4efe7a3956
Jorge Roberto <jorgercj@dcomp.ufs.br>
2021-12-04 02:16:56 -0300

    Refactor screenshot list creation and integration to improve clean code

--- AppStore/AppStore/View/Search/SearchCell.swift
+++ AppStore/AppStore/View/Search/SearchCell.swift
@@ -17,14 +17,10 @@ class SearchCell: UITableViewCell {
             
             imageViewIcon.sd_setImage(with: URL(string: app.iconeUrl), completed: nil)
             if let screenshotUrls = app.screenshotUrls {
-                if screenshotUrls.count > 0 {
-                    self.firstScreenshot.sd_setImage(with: URL(string: app.screenshotUrls![0]), completed: nil)
-                }
-                if screenshotUrls.count > 1 {
-                    self.secondScreenshot.sd_setImage(with: URL(string: app.screenshotUrls![1]), completed: nil)
-                }
-                if screenshotUrls.count > 2 {
-                    self.thirdScreenshot.sd_setImage(with: URL(string: app.screenshotUrls![2]), completed: nil)
+                for index in 0...2 {
+                    if screenshotUrls.count > index {
+                        self.screenshotsList[index].sd_setImage(with: URL(string: app.screenshotUrls![index]), completed: nil)
+                    }
                 }
             }
         }
@@ -36,9 +32,11 @@ class SearchCell: UITableViewCell {
     let enterpriseLabel: UILabel = .textLabel(text: "Enterprise Name", fontSize: 14, numberOfLines: 2)
     let getButton: UIButton = .getButton()
     
-    let firstScreenshot: UIImageView = .screenshotImageView()
-    let secondScreenshot: UIImageView = .screenshotImageView()
-    let thirdScreenshot: UIImageView = .screenshotImageView()
+    let screenshotsList: [UIImageView] = [
+        .screenshotImageView(),
+        .screenshotImageView(),
+        .screenshotImageView()
+    ]
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -58,17 +56,13 @@ class SearchCell: UITableViewCell {
         headerStackView.spacing = 12
         headerStackView.alignment = .center
         
-        let screenshotStackView = UIStackView(arrangedSubviews: [
-            firstScreenshot,
-            secondScreenshot,
-            thirdScreenshot
-        ])
+        let screenshotStackView = UIStackView(arrangedSubviews: screenshotsList)
         screenshotStackView.spacing = 10
         screenshotStackView.distribution = .fillEqually
         let globalStackView = UIStackView(arrangedSubviews: [headerStackView, screenshotStackView])
         globalStackView.spacing = 16
         globalStackView.axis = .vertical
-
+        
         addSubview(globalStackView)
         globalStackView.fillSuperView(padding: .init(top: 20, left: 20, bottom: 20, right: 20))
     }
