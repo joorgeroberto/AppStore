ea12658fa7112b22edb43143c0f84e02b836e85e
Jorge Roberto <jorgercj@dcomp.ufs.br>
2021-12-04 21:00:48 -0300

    Creating AppsViewController

---
+++ AppStore/AppStore/Controller/AppsViewController/AppsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  AppsViewController.swift
+//  AppStore
+//
+//  Created by Jorge de Carvalho on 04/12/21.
+//
+
+import Foundation
+import UIKit
+
+class AppsViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    let cellID = "cellID"
+    let headerID = "headerID"
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self , forCellWithReuseIdentifier: cellID)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerID")
+    }
+}
+
+extension AppsViewController {
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerID, for: indexPath)
+        
+        header.backgroundColor = .blue
+        
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: view.bounds.width, height: view.bounds.width * 0.8)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.bounds.width, height: CGFloat(250))
+    }
+    
+}
